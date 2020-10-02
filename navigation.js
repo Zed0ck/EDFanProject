3274d21d4bf1888ebd3b37f22c4e2605b7cc3714
Mika Vallin <mika.vallin@gmail.com>
2020-10-02 22:02:47 +0300

    Update navigation.js
    Adding correct links to navigation.

--- navigation.js
+++ navigation.js
@@ -1,4 +1,4 @@
 document.getElementById("navi").innerHTML = "<ul>"
   + "<li><a href='index.html'>Home</a></li>"
-  + "<li><a href='#'>Galnet News</a></li>"
-  + "<li><a href='#'>About</a></li>";
+  + "<li><a href='news.html'>Galnet News</a></li>"
+  + "<li><a href='about.html'>About</a></li>";
